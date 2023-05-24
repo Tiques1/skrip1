6c623215739eeb5c2a67dc0544497404224e83c7
Tiques1 <112898566+Tiques1@users.noreply.github.com>
2023-05-24 11:18:05 +0500

    Update Practice 3.cpp

--- Practice 3.cpp
+++ Practice 3.cpp
@@ -1,42 +1,42 @@
-//#include "stdio.h"
-//#pragma warning (disable: 4996)
-//#include <iostream>
-//
-//int main()
-//{
-//	unsigned char a, b, y;
-//	char x;
-//	bool c, d;
-//	printf("Please, tell me two number:\n");
-//	scanf("%hhu %hhu", &a, &b);
-//	
-//	_asm
-//	{
-//		mov al, a;
-//		mov bl, b;
-//		mov c, 0;
-//		mov d, 0;
-//		add al, bl;
-//		jno sign;
-//		mov c, 1;
-//	sign:
-//		jnc unsign;
-//		mov d, 1;
-//	unsign:
-//		mov x, al;
-//		mov y, al;
-//	}
-//
-//	printf("\na + b = %hhu (unsigned)\n", y);
-//	printf("a + b = %d (signed)\n", x);
-//
-//	if (c == 1)
-//		printf("overflow (signed)\n");
-//	else
-//		printf("no overflow (signed)\n");
-//
-//	if (d == 1)
-//		printf("overflow (unsigned)\n");
-//	else
-//		printf("no overflow (unsigned)\n");
-//}
+#include "stdio.h"
+#pragma warning (disable: 4996)
+#include <iostream>
+
+int main()
+{
+	unsigned char a, b, y;
+	char x;
+	bool c, d;
+	printf("Please, tell me two number:\n");
+	scanf("%hhu %hhu", &a, &b);
+	
+	_asm
+	{
+		mov al, a;
+		mov bl, b;
+		mov c, 0;
+		mov d, 0;
+		add al, bl;
+		jno sign;
+		mov c, 1;
+	sign:
+		jnc unsign;
+		mov d, 1;
+	unsign:
+		mov x, al;
+		mov y, al;
+	}
+
+	printf("\na + b = %hhu (unsigned)\n", y);
+	printf("a + b = %d (signed)\n", x);
+
+	if (c == 1)
+		printf("overflow (signed)\n");
+	else
+		printf("no overflow (signed)\n");
+
+	if (d == 1)
+		printf("overflow (unsigned)\n");
+	else
+		printf("no overflow (unsigned)\n");
+}
