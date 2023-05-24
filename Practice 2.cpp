9dcc54547e5a8c63cd3c87de441856d7d6c4e6de
Tiques1 <112898566+Tiques1@users.noreply.github.com>
2023-05-24 11:18:18 +0500

    Update Practice 2.cpp

--- Practice 2.cpp
+++ Practice 2.cpp
@@ -1,42 +1,42 @@
-﻿//#include <stdio.h>
-//#include <iostream>
-//using namespace std;
-//
-//int inout(string var, bool input) {
-//	int x = 0;
-//	if (!input) {
-//		cout << var << endl;
-//	}
-//	else {
-//		cout << var;
-//		cin >> x;
-//		cout << endl;
-//	}
-//	return x;
-//}
-//
-//int main() {
-//	int a,b,c,d,e, res;
-//
-//	inout("enter variables for x = (((a + b) * c) - d) / e", false);
-//	a = inout("a = ", true);
-//	b = inout("b = ", true);
-//	c = inout("c = ", true);
-//	d = inout("d = ", true);
-//	e = inout("e = ", true);
-//		
-//	__asm {
-//		mov eax, a
-//		add eax, b
-//		mov edx, c
-//		mul edx
-//		sub eax, d
-//		cdq
-//		idiv e
-//
-//		mov res, eax
-//	}
-//
-//	cout << "x = " << res;
-//}
+﻿#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int inout(string var, bool input) {
+	int x = 0;
+	if (!input) {
+		cout << var << endl;
+	}
+	else {
+		cout << var;
+		cin >> x;
+		cout << endl;
+	}
+	return x;
+}
+
+int main() {
+	int a,b,c,d,e, res;
+
+	inout("enter variables for x = (((a + b) * c) - d) / e", false);
+	a = inout("a = ", true);
+	b = inout("b = ", true);
+	c = inout("c = ", true);
+	d = inout("d = ", true);
+	e = inout("e = ", true);
+		
+	__asm {
+		mov eax, a
+		add eax, b
+		mov edx, c
+		mul edx
+		sub eax, d
+		cdq
+		idiv e
+
+		mov res, eax
+	}
+
+	cout << "x = " << res;
+}
 
